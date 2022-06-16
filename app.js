42966cb6f6f449c51aa332cdeb30d0fad5f8c60c
derekprieur <DPrieur123@gmail.com>
2022-06-15 21:20:05 -0400

    modal practice

---
+++ app.js
@@ -0,0 +1,14 @@
+//using selectors inside the element
+// traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        btns.forEach(function (item) {
+            if (item.parentElement.parentElement.classList.contains('show-text'));
+            item.parentElement.parentElement.classList.remove('show-text');
+        })
+        e.currentTarget.parentElement.parentElement.classList.toggle('show-text');
+    })
+})
